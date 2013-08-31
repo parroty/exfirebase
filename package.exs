640b8df5bbc3ba59cb033926c5c2c365810ff5a6
parroty <parroty00@gmail.com>
2013-08-31 14:35:14 +0900

    fix wording in package.exs

--- package.exs
+++ package.exs
@@ -1,4 +1,4 @@
-Expm.Package.new(name: "exfirebase", description: "Firebase REST API helper library on Elixir",
-                 version: "0.0.1", keywords: ["Elixir","firebase","rest"],
+Expm.Package.new(name: "exfirebase", description: "Firebase REST API library for Elixir",
+                 version: "0.0.1", keywords: ["Elixir","Firebase","REST"],
                  maintainers: [[name: "parroty", email: "parroty00@gmail.com"]],
                  repositories: [[github: "parroty/exfirebase"]])
