fde9acfb4a5dc01304e0c2ac9fe6f8d1ce4263c9
parroty <parroty00@gmail.com>
2013-09-08 16:18:05 +0900

    add coveralls

--- mix.exs
+++ mix.exs
@@ -5,7 +5,8 @@ defmodule ExFirebase.Mixfile do
     [ app: :exfirebase,
       version: "0.0.1",
       elixir: "~> 0.10.2-dev",
-      deps: deps ]
+      deps: deps
+    ] ++ excoveralls_setting
   end
 
   # Configuration for the OTP application
@@ -22,6 +23,19 @@ defmodule ExFirebase.Mixfile do
       {:mock, ">= 0.0.3", github: "parroty/mock"},
       {:httpotion, github: "parroty/httpotion"},
       {:exactor, github: "sasa1977/exactor"},
+      {:excoveralls, github: "parroty/excoveralls"}
+    ]
+  end
+
+  # Settings for excoveralls
+  defp excoveralls_setting do
+    [
+      test_coverage: [tool: ExCoveralls, type: "local"],
+      env: [
+        coveralls_travis:  [test_coverage: [tool: ExCoveralls, type: "travis"]],
+        coveralls_local:   [test_coverage: [tool: ExCoveralls, type: "local"]],
+        coveralls_general: [test_coverage: [tool: ExCoveralls, type: "general"]]
+      ]
     ]
   end
 end
