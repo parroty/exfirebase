039bb276ec4e1e9d18fa202e54c6f2696ecb18f8
parroty <parroty00@gmail.com>
2013-09-23 21:11:39 +0900

    support record operation in ExFirebase.Objects

--- lib/exfirebase/objects.ex
+++ lib/exfirebase/objects.ex
@@ -45,6 +45,52 @@ defmodule ExFirebase.Objects do
     ExFirebase.delete("#{path}/#{key}")
   end
 
+
+  @doc """
+  Get objects from the specified path as the Record format.
+  It returns the list of the records in record_type.
+  """
+  def get_records(path, record_type) when is_atom(record_type) do
+    verify_record_has_id_field(record_type)
+
+    dict = get(path)
+    HashDict.keys(dict) |>
+      Enum.map(&extract_record(dict, &1)) |>
+      ExFirebase.Records.from_tuples(record_type)
+  end
+
+  @doc """
+  Get object from the specified path as the Record format.
+  It returns a records in record_type.
+  """
+  def get_record(path, key, record_type) when is_atom(record_type) do
+    keywords = [{"id", key} | get(path, key)]
+    [keywords] |>
+      ExFirebase.Records.from_tuples(record_type) |>
+      Enum.first
+  end
+
+  @doc """
+  Update the record in the path with the specified record.
+  The record needs to have id field
+  """
+  def patch_record(path, record) when is_record(record) do
+    if record.id == nil do
+      raise "id field is empty for the specified record."
+    end
+    patch(path, record.id, record.update(id: nil).to_keywords)
+  end
+
+  defp extract_record(dict, key) do
+    [{"id", key} | HashDict.fetch!(dict, key)]
+  end
+
+  defp verify_record_has_id_field(record_type) do
+    if record_type.__info__(:functions)[:id] == nil do
+      raise "Specified record #{record_type} does not have :id field."
+    end
+  end
+
   defp parse({"name", name}, data) do
     {name, data}
   end
