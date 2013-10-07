7c80ed4a68452f75ce8051e29733f06db8fd9206
parroty <parroty00@gmail.com>
2013-10-08 00:37:24 +0900

    apply exvcr for records_test.exs and remove ExFirebase.Mock

--- test/records_test.exs
+++ test/records_test.exs
@@ -2,18 +2,22 @@ Code.require_file "test_helper.exs", __DIR__
 
 defmodule ExFirebase.RecordsTest do
   use ExUnit.Case, async: false
-  import Mock
+  import ExVCR.Mock
 
   defrecord Dummy, id: 1, name: "name"
 
-  test_with_mock "get records", ExFirebase.HTTP, [get: fn(url) -> ExFirebase.Mock.request(url) end] do
-    records = [Dummy.new(id: "2", name: "Jack"), Dummy.new(id: "3", name: "Sparrow")]
-    assert(ExFirebase.Records.get("items_records", Dummy) == records)
+  test "get records" do
+    use_cassette "get_items_records", custom: true do
+      records = [Dummy.new(id: "2", name: "Jack"), Dummy.new(id: "3", name: "Sparrow")]
+      assert(ExFirebase.Records.get("items_records", Dummy) == records)
+    end
   end
 
-  test_with_mock "put record", ExFirebase.HTTP, [put: fn(url, data) -> ExFirebase.Mock.request(url, data) end] do
-    record = Dummy.new(id: "2", name: "Jack")
-    assert(ExFirebase.Records.put("items_push_record", [record]) == [{"name", "-INOQPH-aV_psbk3ZXEX"}])
+  test "put record" do
+    use_cassette "get_items_push_record", custom: true do
+      record = Dummy.new(id: "2", name: "Jack")
+      assert(ExFirebase.Records.put("items_push_record", [record]) == [{"name", "-INOQPH-aV_psbk3ZXEX"}])
+    end
   end
 
   test "generate records from tuples" do
