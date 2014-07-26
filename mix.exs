d1b14e8b54eb8569201c44be1d934c2747250758
parroty <parroty00@gmail.com>
2014-07-27 00:35:16 +0900

    Restructure deps in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExFirebase.Mixfile do
     [ app: :exfirebase,
       version: "0.1.0",
       elixir: "~> 0.14.2",
-      deps: deps(Mix.env),
+      deps: deps,
       test_coverage: [tool: ExCoveralls]
     ]
   end
@@ -17,25 +17,14 @@ defmodule ExFirebase.Mixfile do
 
   # Returns the list of dependencies in the format:
   # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
-  def deps(:test) do
-    deps(:dev)
-  end
-
-  def deps(:dev) do
-    deps(:prod) ++
-      [
-        {:mock, github: "parroty/mock", branch: "version"},
-        {:excoveralls, "~> 0.3"},
-        {:exvcr, "~> 0.2"},
-        {:jsex, "~> 2.0"}
-      ]
-  end
-
-  def deps(:prod) do
+  def deps do
     [
       {:ibrowse, github: "cmullaparthi/ibrowse", ref: "866b0ff5aca229f1ef53653eabc8ed1720c13cd6", override: true},
-      {:json, github: "cblage/elixir-json" },
-      {:httpotion, "~> 0.2"}
+      {:jsex, "~> 2.0"},
+      {:httpotion, "~> 0.2"},
+      {:excoveralls, "~> 0.3", only: :dev},
+      {:exvcr, "~> 0.2", only: [:dev, :test]},
+      {:mock, github: "parroty/mock", branch: "version", only: :test}
     ]
   end
 end
