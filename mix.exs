a686231b74688b4441672e0cd00a158c5c10a89d
parroty <parroty00@gmail.com>
2015-11-22 17:21:34 +0900

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExFirebase.Mixfile do
 
   def project do
     [ app: :exfirebase,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.0",
       deps: deps,
       description: description,
